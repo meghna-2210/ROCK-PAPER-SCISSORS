42f773ea89facae4112ca6dc514ab180a6af72c1
meghna-2210 <63159224+meghna-2210@users.noreply.github.com>
2020-07-30 22:35:50 +0530

    Add files via upload
    //RPS Class consists of main method
    //DRIVER class consists of the logic

---
+++ Computer.java
@@ -0,0 +1,10 @@
+package rock_paper_scissors;
+import java.util.Random;
+
+public class Computer {
+	public int getInput() {
+		Random random = new Random();
+		int input = random.nextInt(3) + 1;
+		return input;
+	}
+}
---
+++ Driver.java
@@ -0,0 +1,41 @@
+package rock_paper_scissors;
+
+public class Driver {
+	public static final int ROCK = 1;
+	public static final int PAPER = 2;
+	public static final int SCISSORS = 3;
+
+	//This function will help to display who selected what
+	public static void display(String who, int s) {
+		switch (s) {
+		case Driver.ROCK:
+			System.out.print(who + " selected ROCK  ");
+			break;
+		case Driver.PAPER:
+			System.out.print(who + " selected PAPER  ");
+			break;
+		case Driver.SCISSORS:
+			System.out.print(who + " selected SCISSORS  ");
+			break;
+		default:
+			break;
+		}
+	}
+	//This function contains the actual logic
+	//and help to decide who won
+	public static int compareSelections(int userSelection, int computerSelection) {
+		if(userSelection==computerSelection) 
+			return 0;
+		switch (userSelection) {
+		case ROCK:
+			return (computerSelection == SCISSORS ? 1 : -1);
+
+		case PAPER:
+			return (computerSelection == ROCK ? 1 : -1);
+
+		case SCISSORS:
+			return (computerSelection == PAPER ? 1 : -1);
+		}
+		return 0;
+}
+}
